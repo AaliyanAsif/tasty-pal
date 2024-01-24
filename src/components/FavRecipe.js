509dd7a2f964c6d56831ba0f18a429ef04d471ca
AaliyanAsif <aaliyan876@gmail.com>
2024-01-24 13:14:35 +0500

    conditional render between fav recipe & details

---
+++ src/components/FavRecipe.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function FavRecipe() {
+  return <div>FavRecipe</div>;
+}
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,23 @@
 import React from "react";
 import RecipeList from "./RecipeList";
+import RecipeDetails from "./RecipeDetails";
+import FavRecipe from "./FavRecipe";
 
-export default function Main({ recipe, onSelectRecipe }) {
+export default function Main({ recipe, onSelectRecipe, selectedId, apiKey }) {
   return (
-    <div>
+    <div
+      style={{
+        display: "flex",
+        flexBasis: 1,
+        alignItems: "flex-start",
+      }}
+    >
       <RecipeList recipe={recipe} onSelectRecipe={onSelectRecipe} />
+      {selectedId ? (
+        <RecipeDetails selectedId={selectedId} apiKey={apiKey} />
+      ) : (
+        <FavRecipe />
+      )}
     </div>
   );
 }
