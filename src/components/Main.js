3c0fd1f1b238440c26a8dcf652c49ebed60aaa96
AaliyanAsif <aaliyan876@gmail.com>
2024-01-25 12:54:00 +0500

    fav items stored in local storage

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,12 +1,15 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import RecipeList from "./RecipeList";
 import RecipeDetails from "./RecipeDetails";
 import FavRecipe from "./FavRecipe";
 
 export default function Main({ recipe, apiKey }) {
-  const [fav, setFav] = useState([]);
   const [selectedId, setSelectedId] = useState(null);
 
+  const [fav, setFav] = useState(function () {
+    const storedValue = localStorage.getItem("fav");
+    return JSON.parse(storedValue);
+  });
   function handleSelectRecipe(id) {
     setSelectedId((selectedId) => (selectedId === id ? null : id));
   }
@@ -19,6 +22,9 @@ export default function Main({ recipe, apiKey }) {
     setSelectedId(null);
   }
 
+  useEffect(() => {
+    localStorage.setItem("fav", JSON.stringify(fav));
+  }, [fav]);
   const containerStyle = {
     display: "flex",
     flexBasis: 1,
